a98d024d573e12cad2a794bff81267231ea923d1
Roberto Erick <robertoerickam@gmail.com>
2023-11-04 16:42:05 -0600

    seccion de caracteristicas de la red funcionando correctamente con la direccion ip y el prefijo

--- src/Componentes/Filas/Segunda_FIla/Caracteristicas.jsx
+++ src/Componentes/Filas/Segunda_FIla/Caracteristicas.jsx
@@ -22,33 +22,47 @@ function Caracteristicas(props) {
 
     // Dividir la dirección IP por el punto y tomar el primer elemento
     const primerNumeroIp = direccionIp.split(".")[0];
+    // Variable para inicializar la clase de direccion ip que tenemos
     var clase = ""; 
+    // variable para inicializar la mascara de red que tenemos
     var mascaraDeRed= "";
+    // Dependiendo lo que haya puesto en el input prefijo, va a pasarse como entero a la variable preijo 
     var prefijo = parseInt(calculo, 10);
+    // variable para inicializar la mascara de subred que tenemos
     var mascaraDeSubred = "";
-    
+    // variable para inicializar el resultado del prefijo despues de convertir el octal a decimal (255.128.0.0)
+    var prefijoResultado = 0;
+    // cariable para inicializar el resultado del host total 
+    var calculoHost = 0;
 
-    //Calculo para la mascara de red
+    var calculoSubred = prefijo;
+    
+    //Calculo para la clase y mascara de red
     switch (true) {
         case primerNumeroIp <= 127:
             clase = "A";
             mascaraDeRed = "255.0.0.0";
+            calculoSubred = calculoSubred - 8;
         break;
         case primerNumeroIp <= 191:
             clase = "B";
             mascaraDeRed = "255.255.0.0";
+            calculoSubred = calculoSubred - 16;
         break;
         case primerNumeroIp <= 223:
             clase = "C";
             mascaraDeRed = "255.255.255.0";
+            calculoSubred = calculoSubred - 24;
         break;
         case primerNumeroIp <= 239:
             clase = "D";
             mascaraDeRed = "No se aplica";
+            calculoSubred = calculoSubred - 32;
         break;
         case primerNumeroIp <= 255:
             clase = "E";
             mascaraDeRed = "No se aplica";
+            calculoSubred = calculoSubred - 32;
         break;
         default:
             clase = "No válida";
@@ -56,60 +70,70 @@ function Caracteristicas(props) {
         break;
       }
 
-      //Calculo para la mascara de subred
+      //Calculo para la mascara de subred, llama a la funcion para calcular el prefijo de red 
+      //dependiendo el numero (0 a 32) para saber donde colocar su calculo en la mascara de subred
       switch (true) {
         case prefijo <= 8:
-            prefijo = prefijo;
-        break;
-        case prefijo >= 9:
-            prefijo = prefijo - 8;
-        break;
-        case prefijo >= 17:
-            prefijo = prefijo - 16;
-        break;
-        case prefijo >= 25:
-            prefijo = prefijo - 24;
-        break;
+          prefijoResultado = calculoPrefijo(prefijo);
+          mascaraDeSubred = prefijoResultado+".0.0.0";
+          calculoHost = calculoHost + 24;                 //Se le suman los 0 de los octetos que quedan para calcular los host
+          break;
+        case prefijo >= 9 && prefijo <= 16:
+          prefijoResultado = calculoPrefijo(prefijo - 8); //Se le resta el octal que ya se completo antes para saber como quedara el octal en el que esta
+          mascaraDeSubred = "255."+prefijoResultado+".0.0";
+          calculoHost = calculoHost + 16;                 //Se le suman los 0 de los octetos que quedan para calcular los host
+          break;
+        case prefijo >= 17 && prefijo <= 24:
+          prefijoResultado = calculoPrefijo(prefijo - 16); //Se les resta los octales que ya se completaron antes para saber como quedara el octal en el que esta
+          mascaraDeSubred = "255.255."+prefijoResultado+".0"
+          calculoHost = calculoHost + 8;                  //Se le suman los 0 de los octetos que quedan para calcular los host
+          break;
+        case prefijo >= 25 && prefijo <= 32:
+          prefijoResultado = calculoPrefijo(prefijo - 24); //Se les resta los octales que ya se completaron antes para saber como quedara el octal en el que esta
+          mascaraDeSubred = "255.255.255."+prefijoResultado;
+          break;
       }
 
-      //Nota para mi de la mañana, intenta hacer un recorrido en "resultado" mientras hay un for dentro de 8 a 0, si encuentra un uno, entonces se multiplicara 2 a la potencia
-      //del numero que tenga en ese momento el for, sino seguira como si nada hasta terminar y sumara todo
+      //Aqui se calcula el prefijo para convertirlo en 1 y 0 para dfespues convertirlo en octal y decimal
+      function calculoPrefijo(prefijocalculo) {
+        const longitud = 8;
+      
+        const unos = "1".repeat(prefijocalculo);
+        const ceros = "0".repeat(longitud - prefijocalculo);
+        const resultado = unos + ceros;
 
-    //   const prefijocalculo = prefijo;
-    //   const longitud = 8;
+        calculoHost = calculoHost + (longitud - prefijocalculo); //El host se le suma la cantidad de 0 que tenga esta operacion para usarlo saliendo de esta funcion
       
-    //   const unos = "1".repeat(prefijocalculo);
-    //   const ceros = "0".repeat(longitud - prefijocalculo);
-    //   const resultado = unos + ceros;
+        let potencia = 7;
+        let resultadoPotencia = 0;
       
-    //   // Inicializar la variable para almacenar el resultado de la operación
-    //   let operacionResultado = 0;
+        // Recorrer la cadena "resultado" e imprimir cada valor
+        for (let i = 0; i < resultado.length; i++) {
+          if (resultado[i] === "1") {
+            resultadoPotencia = 2 ** potencia;
+            prefijoResultado += resultadoPotencia;
+          }
+          potencia = potencia - 1;
+        }
+        potencia = 8;
       
-    //   // Recorrer la cadena de derecha a izquierda
-    //   for (let i = resultado.length - 1; i >= 0; i--) {
-    //     // Obtener el carácter en la posición actual
-    //     const caracter = resultado.charAt(i);
-        
-    //     // Si el carácter es "1", realizar la operación y sumar al resultado
-    //     if (caracter === "1") {
-    //       operacionResultado += Math.pow(2, resultado.length - 1 - i);
-    //     }
-    //     // Si el carácter es "0", no hacer nada
-    //   }
-      
-    //   console.log("Resultado de la operación:", operacionResultado);
-      
+        return prefijoResultado;
+      }
+
+      // Para calcular los host totales se necesita establecer la operacion de 2 a la potencia de la cantidad de 0 que quedan en la operacion despues del prefijo
+      var hostTotales = 2 ** calculoHost;
+      var SubredesTotales = 2 ** calculoSubred;
 
     return (
         <Contenedor className="contenedor">
             <Img className="duda" src={Duda} />
             <ul>
                 <li className="parrafo">Clase: {clase}</li>
-                <li className="parrafo">Mascara de red: </li>
-                <li className="parrafo">Mascara de subred:</li>
-                <li className="parrafo">Host Totales:</li>
-                <li className="parrafo">Host utilizables:</li>
-                <li className="parrafo">Subredes totales:</li>
+                <li className="parrafo">Mascara de red: {mascaraDeRed}</li>
+                <li className="parrafo">Mascara de subred: {mascaraDeSubred}</li>
+                <li className="parrafo">Host Totales: {hostTotales}</li>
+                <li className="parrafo">Host utilizables: {hostTotales - 2}</li>
+                <li className="parrafo">Subredes totales: {SubredesTotales}</li>
             </ul>
         </Contenedor>
     );
